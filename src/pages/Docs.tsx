ec64339fa1c40a2f6286bf340572805a3da184b0
nicolacentonze <nicola.centonze@icloud.com>
2024-09-03 21:25:14 +0200

    Home Init

--- src/pages/Docs.tsx
+++ src/pages/Docs.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom'
 const Docs = () => {
 	return (
 		<div className="docs">
-			<div className="docWrapper">
+			<div className="container docWrapper">
 				<Outlet></Outlet>
 			</div>
 		</div>
--- src/widget/Footer.tsx
+++ src/widget/Footer.tsx
@@ -1,4 +1,10 @@
 const Footer = () => {
-	return <div className="footer">Footer</div>
+	return (
+		<div className="footer">
+			<div className="footer-text">
+				Released under the MIT License - Copyright © 2024 Nicola Centonze
+			</div>
+		</div>
+	)
 }
 export default Footer
